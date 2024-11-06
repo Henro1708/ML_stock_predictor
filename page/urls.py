8b8c08dfbce2231f6a1210ca4a1fe741338ede42
Henro1708 <henriqueengelke@gmail.com>
2024-11-06 17:48:23 -0500

    working form

--- page/urls.py
+++ page/urls.py
@@ -4,6 +4,6 @@ from . import views
 # URL conf
 urlpatterns = [
     path('get_prediction/', views.get_prediction),
-    path('check/', view=views.check)
-
+    path('check/', view=views.check),
+    path('form/', view=views.get_name),
 ]
--- page/views.py
+++ page/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from stock_values import ai_predictor
+from form import NameForm
+from django.views.decorators.csrf import ensure_csrf_cookie
+
 # Create your views here.
+stock = 'AAPL'
+prediction = ai_predictor.get_stock_value()
+ret_values = prediction
 
 def get_prediction(request):
-    stock = 'AAPL'
-    prediction = ai_predictor.get_stock_value()
-    ret_values = prediction
     return HttpResponse(ret_values)
 
+@ensure_csrf_cookie
 def check(request):
-    return HttpResponse('test')
+    return render(request, 'get_stock.html', {'result': str(prediction) })
+
+@ensure_csrf_cookie
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, "form.html", {"form": form})
